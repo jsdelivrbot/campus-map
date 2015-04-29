fd9ae47532c8b8370958641868ba2533593718b1
Aaron Dennis <aaron.p.dennis@gmail.com>
2015-04-29 19:58:12 -0400

    new base map

--- js/add-transit.js
+++ js/add-transit.js
@@ -1,15 +1,7 @@
 // CATA publishes live bus locations in JSON format (?) to the URL below
 // http://realtime.catabus.com/InfoPoint/rest/vehicles/getallvehicles
 
-// Right now, this does not work. There's something wrong.
+//POPULATE MYAVAIL PROPERTIES FROM SERVER SIDE WHEN NECESSARY
 
-// If we find a way to request that data and load the result into our web page, we can add points showing buses
-
-// With AJAX, request the data from the URL
-$.ajax({
-  url: 'http://realtime.catabus.com/InfoPoint/rest/vehicles/getallvehicles', 
-  // If we have success with our request, do the following with the response
-  success: function( response ) {
-    console.log( JSON.stringify(response) ); // Print to the console our live bus data.
-  }
-});
+var geojsonLayer = L.geoJson.ajax("http://realtime.catabus.com/InfoPoint/rest/vehicles/getallvehicles",{dataType:"jsonp"});
+console.log(geojsonLayer);
