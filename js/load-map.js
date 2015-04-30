4e1522fa1ca561b2b17b587178cbb79b8439530d
Aaron Dennis <aaron.p.dennis@gmail.com>
2015-04-30 13:17:23 -0400

    adding emergency map

--- js/load-map.js
+++ js/load-map.js
@@ -7,11 +7,13 @@ var southWest = L.latLng(40.7643, -77.9043), // south-west corner of maximum vie
     bounds = L.latLngBounds(southWest, northEast); // combine the two corners to set the bounds
 
 // Add a basic slippy map to the webpage using Leaflet.js and save it as the variable map
-var map = L.mapbox.map('map', 'aarondennis.campus-map', { // Create a new map using the map tiles 'aarondennis.f6516522' in the map div
+var map = L.mapbox.map('map', 'mapbox.high-contrast', { // Create a new map using the map tiles 'aarondennis.f6516522' in the map div
     maxBounds: bounds, // set the maximum pan boundaries to bounds
     maxZoom: 19, // set how far into the map you can zoom to level 19
     minZoom: 15  // set how far out from the map you can zoom to level 15
   }).setView([40.7967, -77.8619], 18); // set the map view to those coordinates and zoom level 18 (Old Main)
+
+var emergencyOverlay = L.mapbox.tileLayer("eis5187.pg44pldi").addTo(map);
 
 L.control.locate().addTo(map); // Adds the geolocate button to the map
 
