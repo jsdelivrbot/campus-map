81003b521993f2f88d03978b9e7825e9cbbe90e9
William Chittester <wcc5043@psu.edu>
2015-07-02 15:18:40 -0400

    working on popup content

--- js/buildings.js
+++ js/buildings.js
@@ -60,7 +60,7 @@ $.getJSON("https://rawgit.com/pennstategeog467/campus-map/gh-pages/data/building
       if (data[i].label === targetName) { // For each point, check if the title of the point matches the target
       var targetID = data[i]["PICTURE ID"]; // Remembers whichever building id it was that matches for use later.
         var buildName = data[i].label;
-        var facilityType = date[i].category;
+        var facilityType = data[i].category;
         break; // Skip the rest of the loop, we already found what we wanted.
       } else {
         console.log('not found'); // If we don't find it, and this should never happen, write in the console that we didn't find it.
